8b2096160aba90e684c3fc80ffd50e7094902330
rahmahsf <rahmahsafa06@gmal.com>
2026-02-03 00:12:53 +0700

    frontend-rekomendasi

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     # path('', views.dashboard, name='dashboard'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('rekomendasi', views.rekomendasi, name='rekomendasi'),
+    path('riwayat', views.riwayat_rekomendasi, name='riwayat'),
+    path('revise', views.revise, name='revise'),
+    path('detail', views.detail, name='detail'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -13,4 +13,28 @@ def dashboard(request):
 
 @login_required(login_url='login')
 def rekomendasi(request):
-    return render(request, 'main/rekomendasi.html')
+    return render(request, "main/rekomendasi.html", {
+        "page_name": "Rekomendasi",
+        "page_title": "Rekomendasi"
+    })
+
+@login_required(login_url='login')
+def riwayat_rekomendasi(request):
+    return render(request, 'main/riwayat-rekomendasi.html',{
+        "page_name": "Riwayat Rekomendasi",
+        "page_title":"Riwayat Rekomendasi"
+    })
+
+@login_required(login_url='login')
+def revise(request):
+    return render(request, 'main/revise.html',{
+        "page_name": "Riwayat Rekomendasi",
+        "page_title":"Riwayat Rekomendasi"
+    })
+
+@login_required(login_url='login')
+def detail(request):
+    return render(request, 'main/detail-data.html',{
+        "page_name": "Riwayat Rekomendasi",
+        "page_title":"Riwayat Rekomendasi"
+    })
