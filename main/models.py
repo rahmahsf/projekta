bb974a2a9c53c9c08448ea654ee6f434bcafaa42
rahmahsf <rahmahsafa06@gmal.com>
2026-02-10 21:58:42 +0700

    update model

--- main/models.py
+++ main/models.py
@@ -14,3 +14,51 @@ class User(AbstractUser):
         max_length=20,
         choices=Role.choices,
     )
+
+class RawatInap(models.Model):
+    
+    class StatusPulang(models.TextChoices):
+        ATAS_PERSETUJUAN_DOKTER = 'Atas Persetujuan Dokter', 'Atas Persetujuan Dokter'
+        MEMBAIK = 'Membaik', 'Membaik'
+        PINDAH_KAMAR = 'Pindah Kamar', 'Pindah Kamar'
+        SEMBUH = 'Sembuh', 'Sembuh'
+        ATAS_PERMINTAAN_SENDIRI = 'Atas Permintaan Sendiri', 'Atas Permintaan Sendiri'
+        MENINGGAL = 'Meninggal', 'Meninggal'
+    
+    no_rawat = models.CharField(max_length=255)
+    tgl_masuk = models.DateTimeField()
+    tgl_keluar = models.DateTimeField(null=True, blank=True)
+    stts_pulang = models.CharField(
+        max_length=50,
+        choices=StatusPulang.choices,
+        null=True,
+        blank=True
+    )
+    
+    class Meta:
+        db_table = 'rawat_inap'
+
+class Kasus(models.Model):
+    bulan = models.CharField(max_length=4)
+    tahun = models.CharField(max_length=5)
+    bor = models.FloatField()
+    los = models.FloatField()
+    gdr = models.FloatField()
+    
+    class Meta:
+        db_table = 'kasus'
+
+class Rekomendasi(models.Model):
+    
+    class JenisRekomendasi(models.TextChoices):
+        PELAYANAN_MEDIS = 'pelayanan medis', 'Pelayanan Medis'
+        KEPEGAWAIAN = 'kepegawaian', 'Kepegawaian'
+    
+    rekomendasi = models.TextField()
+    jenis_rekomendasi = models.CharField(
+        max_length=20,
+        choices=JenisRekomendasi.choices
+    )
+    
+    class Meta:
+        db_table = 'rekomendasi'
