34b21905949e07df0af6e7a0316e6f2cba020491
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2026-02-25 01:44:50 +0700

    fix seed and readme

--- main/management/commands/seed_kasus_rekomendasi.py
+++ main/management/commands/seed_kasus_rekomendasi.py
@@ -9,23 +9,30 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         df = pd.read_csv('dataset/df_merge.csv')
 
+        mapping_rekomendasi = {
+            'Kerja Sama': 'Meningkatkan Kerja Sama dengan instansi lain',
+            'Triase IGD': 'Memperbaiki Triase IGD',
+            'Sistem Rujukan': 'Memperbaiki Sistem Rujukan',
+            'Alat Medis': 'Penambahan Alat Medis',
+            'Pelatihan Dokter': 'Pelatihan Dokter',
+            'Kualifikasi Staf': 'Perbaikan Kualifikasi Staf',
+            'Pelayanan Pasien': 'Perbaikan Pelayanan Pasien'
+        }
+
         for _, row in df.iterrows():
-            kasus = Kasus.objects.get(
-                bulan=row['bulan'],
-                tahun=row['tahun']
+            kasus, created = Kasus.objects.get_or_create(
+                bulan=str(row['bulan']),
+                tahun=str(row['tahun']),
+                defaults={
+                    'bor': row['BOR'],
+                    'los': row['LOS'],
+                    'gdr': row['GDR']
+                }
             )
 
-            for kolom in [
-                'Kerja Sama',
-                'Triase IGD',
-                'Sistem Rujukan',
-                'Alat Medis',
-                'Pelatihan Dokter',
-                'Kualifikasi Staf',
-                'Pelayanan Pasien'
-            ]:
+            for kolom, teks_rekomendasi in mapping_rekomendasi.items():
                 if row[kolom] == True:
-                    rekom = Rekomendasi.objects.get(rekomendasi=kolom)
+                    rekom = Rekomendasi.objects.get(rekomendasi=teks_rekomendasi)
 
                     KasusRekomendasi.objects.get_or_create(
                         kasus=kasus,
